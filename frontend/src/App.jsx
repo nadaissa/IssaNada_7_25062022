e34bd1d94ebd855ef8dc933fdb97ddf2016fd285
Nada Issa <rosariojesus909@gmail.com>
2022-05-29 22:26:34 +0200

    :art: :lipstick: :construction: improved initial styling structure - added components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
             <Header />
             <Routes>
             <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/login" element={<Login/>}/>
+            <Route exact path="/Login" element={<Login/>}/>
+            <Route exact path="/Signup" element={<Signup/>}/>
             </Routes>
             <Footer />
         </Router>
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Signup() {
+    return (
+       <div>Please signup</div>
+    )
+}
+
+export default Signup;
