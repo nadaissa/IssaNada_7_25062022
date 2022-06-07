b147bd0dc492445225d45fe758421d565008e544
Nada Issa <rosariojesus909@gmail.com>
2022-06-07 17:31:42 +0200

    improved layout & identity,displayed home page content [need to improve responsive]

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Link} from "react-router-dom";
 function Footer() {
     return (
-        <div className="footer">            
-            <Link className="footer__link" to="/">Contact</Link>
-            <Link className="footer__link" to="/">A propos</Link>
-            <Link className="footer__link" to="/">Mentions légales</Link>
-        </div>
+        <footer className="footer">
+            <div className="footer-list" aria-label="navigation bas de page">            
+                <Link className="footer-list__link" to="/">Contact</Link>
+                <Link className="footer-list__link" to="/">A propos</Link>
+                <Link className="footer-list__link" to="/">Mentions légales</Link>
+            </div>
+        </footer>
     )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,38 @@ import React from "react";
 
 function Home() {
     return (
-       <div className="main-content">This is home page</div>
+       <div className="main-content-wrapper" aria-label="contenu principal">
+           <div className="home-parags" aria-label="présentation de la plateforme Groupomania">
+               <h1 className="home-parags__h1" aria-label="titre de bienvenue">Bienvenue à notre plateforme!</h1>
+               <h2 className="home-parags__h2">Lorem ipsum 1</h2>
+               <p className="home-parags__p">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget egestas nibh. 
+               Mauris vitae fermentum nisl. Aenean semper porttitor risus. Morbi mollis venenatis ultrices. 
+               Duis euismod sagittis ligula vitae auctor. Phasellus nec odio congue, sagittis ligula id, ornare enim. 
+               Proin porttitor lacus nec nisl consequat, sed mollis turpis mollis. Suspendisse vehicula erat non semper varius. 
+               Cras aliquam, lorem vel euismod ultrices, nulla lectus semper lacus, vitae suscipit nisl eros et eros. 
+               Morbi vehicula, nunc eget tincidunt interdum, lacus ex condimentum leo, vel porttitor risus massa nec massa.
+               </p>
+               <h2 className="home-parags__h2">Lorem ipsum 2</h2>
+               <p className="home-parags__p">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget egestas nibh. 
+               Mauris vitae fermentum nisl. Aenean semper porttitor risus. Morbi mollis venenatis ultrices. 
+               Duis euismod sagittis ligula vitae auctor. Phasellus nec odio congue, sagittis ligula id, ornare enim. 
+               Proin porttitor lacus nec nisl consequat, sed mollis turpis mollis. Suspendisse vehicula erat non semper varius. 
+               Cras aliquam, lorem vel euismod ultrices, nulla lectus semper lacus, vitae suscipit nisl eros et eros. 
+               Morbi vehicula, nunc eget tincidunt interdum, lacus ex condimentum leo, vel porttitor risus massa nec massa.
+               </p>
+               <h2 className="home-parags__h2">Lorem ipsum 3</h2>
+               <p className="home-parags__p">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget egestas nibh. 
+               Mauris vitae fermentum nisl. Aenean semper porttitor risus. Morbi mollis venenatis ultrices. 
+               Duis euismod sagittis ligula vitae auctor. Phasellus nec odio congue, sagittis ligula id, ornare enim. 
+               Proin porttitor lacus nec nisl consequat, sed mollis turpis mollis. Suspendisse vehicula erat non semper varius. 
+               Cras aliquam, lorem vel euismod ultrices, nulla lectus semper lacus, vitae suscipit nisl eros et eros. 
+               Morbi vehicula, nunc eget tincidunt interdum, lacus ex condimentum leo, vel porttitor risus massa nec massa.
+               </p>
+           </div>
+       </div>
     )
 }
 
