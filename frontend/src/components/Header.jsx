280d57ed573f41801b97e2678b9267121fad1fc3
Nada Issa <rosariojesus909@gmail.com>
2022-06-07 17:31:42 +0200

    :art: improved layout & identity,displayed home page content [need to improve responsive]

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,44 +1,51 @@
 import React from "react";
 import logo from "../assets/images/icon-left-font-monochrome-white.svg";
 import {Link} from "react-router-dom";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faBell, faMagnifyingGlass, faMessage, faUser } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowRightToBracket, faHouse, faUserPlus} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
-        <nav className="header">
-            <div className="header-left">
+        <header className="header">
+            <nav className="header-nav" aria-label="navigation en-tête">
+                <Link className="header-left" to="/">
                 <img src={logo} alt="logo groupomania" className="header-left__logo" />
-            </div>
-            {/*<div className="header-center">
-               <div className="header-center__searchBar">
-                    <FontAwesomeIcon icon={faMagnifyingGlass} className="header-center__icon"/>
-                    <input placeholder="Search for friends or content!" className="header-center__input" />
+                </Link>
+                {/*<div className="header-center">
+                <div className="header-center__searchBar">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="header-center__icon"/>
+                        <input placeholder="Search for friends or content!" className="header-center__input" />
+                    </div>
+                </div>*/}
+                <div className="header-right">
+                    <div className="header-right__links">
+                        <Link className= "header-right__link" to="/" aria-label="icone d'accueil">
+                            <FontAwesomeIcon icon={faHouse} className="header-right__icon"/>
+                        </Link>
+                        <Link className= "header-right__link" to="/Login" aria-label="connexion">
+                            <FontAwesomeIcon icon={faUserPlus} className="header-right__icon"/>
+                        </Link>
+                        <Link className= "header-right__link" to="/Signup" aria-label="inscription">
+                            <FontAwesomeIcon icon={faArrowRightToBracket} className="header-right__icon"/>
+                        </Link>
+                    </div>
+                    {/*<div className="header-icons">
+                        <FontAwesomeIcon icon={faUser} className="header-icons__icon"/>
+                        <span className="header-icons__badge">1</span>
+                    </div>
+                    <div className="header-icons">
+                        <FontAwesomeIcon icon={faMessage} className="header-icons__icon"/>
+                        <span className="header-icons__badge">1</span>
+                    </div>
+                    <div className="headerIcons">
+                        <FontAwesomeIcon icon={faBell} className="header-icons__icon"/>
+                        <span className="header-icons__badge">1</span>
+        </div>*/}
                 </div>
-            </div>*/}
-            <div className="header-right">
-                <div className="header-right__links">
-                    <Link className="header-right__link" to="/">Accueil</Link>
-                    <Link className= "header-right__link" to="/Login">Login</Link>
-                    <Link className= "header-right__link" to="/Signup">Signup</Link>
-                </div>
-                {/*<div className="header-icons">
-                    <FontAwesomeIcon icon={faUser} className="header-icons__icon"/>
-                    <span className="header-icons__badge">1</span>
-                </div>
-                <div className="header-icons">
-                    <FontAwesomeIcon icon={faMessage} className="header-icons__icon"/>
-                    <span className="header-icons__badge">1</span>
-                </div>
-                <div className="headerIcons">
-                    <FontAwesomeIcon icon={faBell} className="header-icons__icon"/>
-                    <span className="header-icons__badge">1</span>
-    </div>*/}
-            </div>
-            {/*<img src="" alt="profil" className="header-profil" />*/}
-            
-            
-        </nav>
+                {/*<img src="" alt="profil" className="header-profil" />*/}
+                
+            </nav>
+        </header>    
     
     )
 }
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/main.scss'
+
 import {createRoot} from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
