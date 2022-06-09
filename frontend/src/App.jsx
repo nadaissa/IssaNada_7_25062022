0c872c284a3183e49a47edd19a58a171e2651ddf
Nada Issa <rosariojesus909@gmail.com>
2022-06-09 16:18:20 +0200

    :bug: :construction: :twisted_rightwards_arrows: resolved mergin error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,8 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-<<<<<<< HEAD
-
-=======
 import Contact from './pages/Contact'
 import Mentions from './pages/Mentions'
->>>>>>> dev
 
 function App() {
     
@@ -21,11 +17,8 @@ function App() {
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/Login" element={<Login/>}/>
             <Route exact path="/Signup" element={<Signup/>}/>
-<<<<<<< HEAD
-=======
             <Route exact path="/Contact" element={<Contact/>}/>
             <Route exact path='/Mentions' element={<Mentions/>}/>
->>>>>>> dev
             </Routes>
             <Footer />
         </Router>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,14 +4,8 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="footer-list" aria-label="navigation bas de page">            
-<<<<<<< HEAD
-                <Link className="footer-list__link" to="/">Contact</Link>
-                <Link className="footer-list__link" to="/">A propos</Link>
-                <Link className="footer-list__link" to="/">Mentions légales</Link>
-=======
                 <Link className="footer-list__link" to="/Contact">Contact</Link>
                 <Link className="footer-list__link" to="/Mentions">Mentions légales</Link>
->>>>>>> dev
             </div>
         </footer>
     )
